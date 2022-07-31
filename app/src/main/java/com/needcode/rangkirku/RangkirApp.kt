9d45fbfaaf21f3276470aa0fedb6973296a73a1d
azerid <antareza08@gmail.com>
2022-07-31 18:39:38 +0700

    No mode night

--- app/src/main/java/com/needcode/rangkirku/RangkirApp.kt
+++ app/src/main/java/com/needcode/rangkirku/RangkirApp.kt
@@ -1,11 +1,13 @@
 package com.needcode.rangkirku
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import timber.log.Timber
 
 class RangkirApp : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
