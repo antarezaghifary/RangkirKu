21462e514fd318af7c5e8accfab8761085f9b95e
azerid <antareza08@gmail.com>
2022-07-31 11:00:46 +0700

    Add Waybill

---
+++ app/src/main/java/com/needcode/rangkirku/ui/waybill/WayBillFragment.kt
@@ -0,0 +1,19 @@
+package com.needcode.rangkirku.ui.waybill
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.needcode.rangkirku.R
+
+class WayBillFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_way_bill, container, false)
+    }
+}
