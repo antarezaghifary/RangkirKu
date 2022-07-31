bc4b1fb4fd4877a84782348e6060b80e3ca81511
azerid <antareza08@gmail.com>
2022-07-31 15:43:43 +0700

    Add Api service & Resource -> { Handling State }

---
+++ app/src/main/java/com/needcode/rangkirku/network/ApiService.kt
@@ -0,0 +1,36 @@
+package com.needcode.rangkirku.network
+
+import com.google.gson.GsonBuilder
+import com.needcode.rangkirku.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+object ApiService {
+
+    fun getClient(): Retrofit {
+
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .addInterceptor { chain ->
+                val request = chain.request().newBuilder()
+                    .addHeader("key", BuildConfig.API_KEY).build()
+                chain.proceed(request)
+            }
+            .build()
+
+        val gson = GsonBuilder().serializeNulls().create()
+
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .client(client)
+            .build()
+    }
+
+}
