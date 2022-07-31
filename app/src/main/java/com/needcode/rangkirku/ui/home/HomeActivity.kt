6a802c0a329a452d2a39b3a138968285a6a976bc
azerid <antareza08@gmail.com>
2022-07-31 10:57:44 +0700

    Add layout home

---
+++ app/src/main/java/com/needcode/rangkirku/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.needcode.rangkirku.ui.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.needcode.rangkirku.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
