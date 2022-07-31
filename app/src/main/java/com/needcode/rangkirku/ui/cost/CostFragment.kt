76e14c079fbe6a99f330043e975a269912e74c3c
azerid <antareza08@gmail.com>
2022-07-31 14:34:18 +0700

    Add fragment cost

--- app/src/main/java/com/needcode/rangkirku/ui/cost/CostFragment.kt
+++ app/src/main/java/com/needcode/rangkirku/ui/cost/CostFragment.kt
@@ -1,20 +1,34 @@
 package com.needcode.rangkirku.ui.cost
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.needcode.rangkirku.R
+import com.needcode.rangkirku.databinding.FragmentCostBinding
+import com.needcode.rangkirku.ui.city.CityActivity
 
 class CostFragment : Fragment() {
+
+    private lateinit var binding: FragmentCostBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cost, container, false)
+        binding = FragmentCostBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            editOrigin.setOnClickListener {
+                startActivity(Intent(context, CityActivity::class.java))
+            }
+        }
     }
 
 }
