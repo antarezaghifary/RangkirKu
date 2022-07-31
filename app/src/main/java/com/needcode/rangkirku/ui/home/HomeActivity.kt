0f6cfcd90de46d531d8ad276a9fa62ffe40b6a7d
azerid <antareza08@gmail.com>
2022-07-31 11:12:42 +0700

    Setup Tab layout in HomeActivity

--- app/src/main/java/com/needcode/rangkirku/ui/home/HomeActivity.kt
+++ app/src/main/java/com/needcode/rangkirku/ui/home/HomeActivity.kt
@@ -2,11 +2,36 @@ package com.needcode.rangkirku.ui.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.needcode.rangkirku.R
+import com.google.android.material.tabs.TabLayoutMediator
+import com.needcode.rangkirku.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityHomeBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(binding.root)
+
+        with(binding) {
+
+        }
+
+        setupTab()
+    }
+
+    private fun setupTab() {
+        val title = arrayListOf("CEK ONGKIR", "CEK RESI")
+        val tabAdapter = HomeTabAdapter(supportFragmentManager, lifecycle)
+
+        with(binding) {
+            viewPager.adapter = tabAdapter
+            TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+                tab.text = title[position]
+            }.attach()
+        }
+
     }
 }
